3a770963c787caa16a0077b5cdb5efd5a8c91e02
Samuel Façanha <samuelfacanha@edu.unifor.br>
2021-11-24 11:41:59 -0300

    conectando com o mongo na nuvem

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const conectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost/zeus', { 
-            useNewUrlParser: true, useUnifiedTopology: true 
-        });
-        mongoose.Promise = global.Promise;
-    } catch (err) {
-        process.exit(1)
-    }
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@cluster0.ev4cf.mongodb.net/Cluster0?retryWrites=true&w=majority",
+      //'mongodb://localhost/zeus'
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    mongoose.Promise = global.Promise;
+  } catch (err) {
+    process.exit(1);
+  }
+};
 
 module.exports = conectDB;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const dataBase = require('./database/index')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dataBase = require("./database/index");
 
 const app = express();
-dataBase()
+dataBase();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
-app.options('*', cors());
-app.all('*', require('./routes/index'))
+app.options("*", cors());
+app.all("*", require("./routes/index"));
 
-app.listen(3000);
+app.listen(4000);
